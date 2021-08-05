9f94694b8f1484029ce260089afdf78c20ab9546
andriibodryi <lexoveri@gmail.com>
2021-08-05 11:59:35 +0300

    export createAsyncaction from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './selectors';
 export { requestStatuses } from './reducer';
 export * from './hooks';
+export * from './actionCreators';
 
 export type { ActionType, MultiProcessActionType } from './model';
